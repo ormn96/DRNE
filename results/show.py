b08e9068e8da53714afb1f587465a52338d16a1e
or man <Ormn1996@gmail.com>
2021-05-25 10:33:55 +0300

    upload result

--- results/show.py
+++ results/show.py
@@ -1,3 +1,5 @@
 import numpy as np
 
 loaded = np.load("C:\\Users\\User\\OneDrive\\בראודה\\כרייה\\project\\DRNE\\results\\result\\barbell\\test_16_0.0_0.5\\embeddings.npy")
+loaded2 = np.load("C:\\Users\\User\\OneDrive\\בראודה\\כרייה\\project\\DRNE\\results\\result\\karate\\test_16_0.0_0.5\\embeddings.npy")
+
